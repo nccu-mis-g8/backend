d8a9578d8f975c2704c4e7569642d01e7e37c850
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-09-19 14:44:29 +0800

    add training_file model

---
+++ models/training_file.py
@@ -0,0 +1,14 @@
+from extensions import db
+import uuid
+
+
+class TrainingFile(db.Model):
+    __tablename__ = "training_file"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    filename = db.Column(db.String(36), nullable=False)
+
+    # 上傳file後先生成TrainingFile物件，再從TrainingFile object拿filename做為檔名存file。
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.filename = uuid.uuid4()
