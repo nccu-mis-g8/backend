e5914cdc134a0992d042992d73e696a608bf379e
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-09-21 15:55:34 +0800

    fix trained_model & training_file

--- models/training_file.py
+++ models/training_file.py
@@ -7,8 +7,10 @@ class TrainingFile(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     filename = db.Column(db.String(36), nullable=False)
+    is_trained: bool = db.Column(db.Boolean, default=False, nullable=False)
 
     # 上傳file後先生成TrainingFile物件，再從TrainingFile object拿filename做為檔名存file。
     def __init__(self, user_id):
         self.user_id = user_id
         self.filename = str(uuid.uuid4()) + ".csv"
+        self.is_trained = False
