3cee733eccc0db7ff6ed5d6e273115af392b706a
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-09-24 00:16:54 +0800

    attempt to fix "Object of type LLMTrainingArg is not JSON serializable"

--- train_model/llm_training_arg.py
+++ train_model/llm_training_arg.py
@@ -6,3 +6,11 @@ class LLMTrainingArg:
         self.saved_model_dir = saved_model_dir
         self.output_dir = output_dir
         self.data_path = data_path
+
+    def to_dict(self):
+        return {
+            "model_dir": self.model_dir,
+            "output_dir": self.output_dir,
+            "data_path": self.data_path,
+            "saved_model_dir": self.saved_model_dir,
+        }
