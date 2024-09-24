698ed220812ed3c96a3feb0f6dfc423e0adfbee7
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-09-24 14:21:16 +0800

    attempt to fix no object to concatenate

--- utils/merge_csv_files.py
+++ utils/merge_csv_files.py
@@ -11,7 +11,10 @@ def merge_csv_files(file_list: List[str]) -> str:
     file_name = str(uuid.uuid4())
     output_file = os.path.join(FILE_DIRECTORY, file_name)
     # Read and concatenate all CSV files
-    df_list = [pd.read_csv(file) for file in file_list if os.path.exists(file)]
+    df_list = []
+    for file in file_list:
+        if os.path.exists(file):
+            df_list.append(file)
     merged_df = pd.concat(df_list, ignore_index=True)
 
     merged_df.to_csv(output_file, index=False)
