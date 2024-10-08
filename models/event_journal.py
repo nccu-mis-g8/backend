c071f0cee4b96a4f8ae1cf375cb15efac31b414c
ndl1n <a0938391667@gmail.com>
2024-10-08 15:22:09 +0800

    add EventJournal model

---
+++ models/event_journal.py
@@ -0,0 +1,18 @@
+from sqlalchemy import DateTime, func
+from extensions import db
+
+
+class EventJournal(db.Model):
+    __tablename__ = 'event_journal'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # 假設你有一個 users 表
+    event_title = db.Column(db.String(255), nullable=False)
+    event_content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(DateTime(timezone=True), default=func.now(), nullable=False)
+    updated_at = db.Column(DateTime(timezone=True), default=func.now(), onupdate=func.now(), nullable=False)
+    
+    def __init__(self, user_id,event_title, event_content):
+        self.user_id = user_id
+        self.event_title = event_title
+        self.event_content = event_content
