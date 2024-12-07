f271bd019c762d84d35f7f00cd57fae034594ef6
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-12-07 23:04:41 +0800

    fix cors

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app.config["SWAGGER"] = {
 }
 
 # initialize
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:7664", "http://127.0.0.1:5000"]}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 db.init_app(app)
 jwt.init_app(app)
 
