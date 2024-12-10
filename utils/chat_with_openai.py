e143a5007d30dd370667ca4038cd17d0650a47e0
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-12-10 17:06:22 +0800

    add chat_with_openai

---
+++ utils/chat_with_openai.py
@@ -0,0 +1,15 @@
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def get_response_from_openai(chat_template):
+    try:
+        response = openai.chat.completions.create(model="gpt-4", messages=chat_template)
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"發生錯誤: {str(e)}")
+        return None
